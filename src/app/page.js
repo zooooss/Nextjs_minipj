6ca8b799ea8c89da627b972b3553268cd0c6a3b9
zooooss <wldn000829@gmail.com>
2023-09-14 14:40:59 +0900

    loading static asset

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
-import Image from 'next/image'
+import Image from 'next/image';
 
 export default function Home() {
-  return (
-    <>
-    <h2>Welcome</h2>
-    Hello, WEB!
-    </>
-  )
+    return (
+        <>
+            <h2>Welcome</h2>
+            Hello, WEB!
+            <br></br>
+            <img src="/monkey.png"></img>
+        </>
+    );
 }
